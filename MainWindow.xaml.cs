ab696dcf53524f37d3f0d0acd7a0ede802e70980
Jakub-Chrzanowski <132926284+Jakub-Chrzanowski@users.noreply.github.com>
2025-04-15 15:17:27 +0200

    Aktualizacja zbiorcza

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+using System;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,48 +12,85 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
-   
+
 
 
 namespace WpfApp1
 {
-    public class Person { 
+    public class Person
+    {
+        public string? m_strPESEL { get; set; }
+        public string? m_strName { get; set; }
+        public string? m_strAnotherName { get; set; }
+        public string? m_strSurname { get; set; }
 
-    public int m_nID { get; set; }
-    public string m_strName { get; set; }
-    public string m_strSname { get; set; }
-    public string m_strPESEL { get; set; }
-}
-    
-        public partial class MainWindow : Window
+        public string? m_strBirthDate { get; set; }
+
+        public string? m_strPhoneNumber { get; set; }
+
+        public string? m_strAdress { get; set; }
+        public string? m_strLocality { get; set; }
+
+        public string? m_strZipCode { get; set; }
+        public Person()
         {
-            
-            public ObservableCollection<Person> People { get; set; }
 
-            public MainWindow()
-            {
-                InitializeComponent();
-
-            
-                People = new ObservableCollection<Person>();
-                listView.ItemsSource = People;
-            }
-
-            
-            private void Button_Click(object sender, RoutedEventArgs e)
-            {
-               
-                Random random = new Random();
-                Person newPerson = new Person
-                {
-                    m_nID = random.Next(1000, 9999), 
-                    m_strName = "Imie" + random.Next(1, 100), 
-                    m_strSname = "Nazwisko" + random.Next(1, 100), 
-                    m_strPESEL = random.Next(1000000, 99999999).ToString() 
-                };
-
-              
-                People.Add(newPerson);
-            }
         }
     }
+
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Person> People { get; set; }
+        WindowData window = new WindowData();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            People = new ObservableCollection<Person>();
+            listView.ItemsSource = People;
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            Person newPerson = new Person
+            {
+                m_strPESEL = window.PESEL.Text,
+                m_strName = ,
+                m_strAnotherName = ,
+                m_strSurname = "Nazwisko" + random.Next(1, 100),
+                m_strBirthDate = ,
+                m_strPhoneNumber = ,
+                m_strAdress = ,
+                m_strLocality = ,
+                m_strZipCode = ,
+            };
+
+
+            People.Add(newPerson);
+            window.Show();
+
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void NewRecord_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void RemoveSel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
